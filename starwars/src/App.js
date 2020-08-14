f35867c4fa379a9e531189d38fd76504736318cd
Andrew Ruiz <nukedoom@gmail.com>
2020-08-14 09:50:03 -0700

    Added theme and mappings

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect}from 'react';
 import './App.css';
 
 import axios from 'axios'
+import Character from './components/Character'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -15,9 +16,10 @@ const App = () => {
 
   useEffect(() => {
 
-    axios.get(`https://swapi.dev/api/people/1`)
+    axios.get(`https://swapi.dev/api/people/`)
       .then(response => {
-        console.log(response.data)
+        console.log(response.data.results)
+        setData(response.data.results)
       })
       .catch(error => {
         debugger
@@ -27,7 +29,17 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+
+      {
+        data.map( elem => {
+          console.log(elem)
+          return <Character data={elem} />
+        })
+      }
+
+      {/* {<Character data={data}/>} */}
     </div>
+  
   );
 }
 
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,28 @@
 // Write your Character component here
+
+import React from 'react';
+import styled from 'styled-components'
+
+const StyledCharacter = styled.div`
+    display: flex;
+    justify-content: center;
+
+    .characterName {
+        color: purple;
+    }
+`
+
+const Character = (props) => {
+    const {name} = props.data
+
+    return (
+        <StyledCharacter>
+        <div className="characterName">
+            <p>{name}</p>
+        </div>
+        </StyledCharacter>
+     
+    )
+}
+
+export default Character;
